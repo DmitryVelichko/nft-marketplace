e75f578c3f7d599f309d663f7496a4c6961fee11
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-23 00:03:21 +0800

    update Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+
+import images from '../assets'
+import { Button } from './'
 
 const Footer = () => (
-  <div>Footer</div>
+  const { theme } = useTheme()
+
+return (
+  <footer className='flexCenter flex-col border-t dark:border-nft-black-1 border-nft-gray-1 sm:py-8 py-16'>
+    <div className='flexCenter w-full mt-5 border-t dark:border-nft-black-1 border-nft-gray-1 sm:px-4 px-16'>
+
+    </div>
+  </footer>
+)
 );
 
 export default Footer;
