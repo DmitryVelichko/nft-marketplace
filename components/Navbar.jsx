3d848d1700aa26ee5054dc67365f74e501a174e8
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-27 16:35:06 +0800

    Add Banner

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import images from '../assets';
-// import { NFTContext } from '../context/NFTContext';
+import { NFTContext } from '../context/NFTContext';
 import Button from './Button';
 
 const MenuItems = ({ isMobile, active, setActive, setIsOpen }) => {
