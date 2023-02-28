bdc26043f26b28e478a9f99f75b36714804c5187
Dmitry Velichko <soleil-2005@mail.ru>
2023-02-28 11:43:18 +0800

    Add NFTCard

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -18,4 +18,5 @@ const NFTCard = ({ nft, onProfilePage }) => {
         <div className="mt-3 flex flex-col">
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm minlg:text-xl">{nft.name}</p>
           <div className="flexBetween mt-1 minlg:mt-3 flex-row xs:flex-col xs:items-start xs:mt-3">
+            <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xs minlg:text-lg">{nft.price}<span className="font-normal"> {nftCurrency}</span></p>
        
