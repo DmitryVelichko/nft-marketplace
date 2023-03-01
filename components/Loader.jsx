ca94b8cbf1c63ddb2c59247c6fc4eee58875cadb
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-01 13:57:20 +0800

    Add Loader

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -2,3 +2,6 @@ import Image from 'next/image';
 
 import images from '../assets';
 
+const Loader = () => (
+  <div className="flexCenter w-full my-4">
+    <Image src={images.loader} alt="loader" width={100} 
