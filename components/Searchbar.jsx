db8d5fe93d7d8313340a34c037792b6ca05b6107
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-02 09:32:32 +0800

    Add Searchbar

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -5,6 +5,24 @@ import { useTheme } from 'next-themes';
 import images from '../assets';
 
 const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch }) => {
+  const [search, setSearch] = useState('');
+  const [toggle, setToggle] = useState(false);
+  const [debouncedSearch, setDebouncedSearch] = useState(search);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const timer = setTimeout(() => setSearch(debouncedSearch), 1000);
+
+    return () => clearTimeout(timer);
+  }, [debouncedSearch]);
+
+  useEffect(() => {
+    if (search) {
+      handleSearch(search);
+    } else {
+      clearSearch();
+    }
+  }, [search]);
 
 
 export default SearchBar;
