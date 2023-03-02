a721bee1c794ea613b1e4e957d1cf8cdfe6b0ca6
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-02 09:32:18 +0800

    Add Searchbar

---
+++ components/Searchbar.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+
+import images from '../assets';
+
+const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch }) => {
+
+
+export default SearchBar;
