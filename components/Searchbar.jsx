09595c5e6be924094f154b7dc5fe776cb56d3dc6
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-02 09:33:06 +0800

    Add Searchbar

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -58,5 +58,22 @@ const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
           className={theme === 'light' ? 'filter invert' : undefined}
         />
 
+        {toggle && (
+          <div className="absolute top-full left-0 right-0 w-full mt-3 z-10 dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md">
+            {['Recently added', 'Price (low to high)', 'Price (high to low)'].map((item) => (
+              <p
+                className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs my-3 cursor-pointer"
+                onClick={() => setActiveSelect(item)}
+                key={item}
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default SearchBar;
