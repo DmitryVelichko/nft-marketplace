ddd1d023b8f246039eb66301dc84be62c216e0fd
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-02 09:32:59 +0800

    Add Searchbar

--- components/Searchbar.jsx
+++ components/Searchbar.jsx
@@ -44,6 +44,19 @@ const SearchBar = ({ activeSelect, setActiveSelect, handleSearch, clearSearch })
         />
       </div>
 
+      <div
+        onClick={() => setToggle(!toggle)}
+        className="relative flexBetween ml-4 sm:ml-0 sm:mt-2 min-w-190 cursor-pointer dark:bg-nft-black-2 bg-white border dark:border-nft-black-2 border-nft-gray-2 py-3 px-4 rounded-md"
+      >
+        <p className="font-poppins dark:text-white text-nft-black-1 font-normal text-xs">{activeSelect}</p>
+        <Image
+          src={images.arrow}
+          objectFit="contain"
+          width={15}
+          height={15}
+          alt="arrow"
+          className={theme === 'light' ? 'filter invert' : undefined}
+        />
 
 
 export default SearchBar;
