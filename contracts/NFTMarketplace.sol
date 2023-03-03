b72e4c1070cd64487f85f1767eac7c5c62743e4b
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-03 08:16:47 +0800

    Add smartcontract NFTMarketplace.sol

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -20,3 +20,34 @@ contract NFTMarketplace is ERC721URIStorage {
     // and then using counters this we'll increment token ids
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
+
+    // fee to list an nft on the marketplace
+    // charge a listing fee.
+    uint256 listingPrice = 0.025 ether;
+
+    // declaring the owner of the contract
+    // owner earns a commision on every item sold
+    address payable owner; 
+
+    // keeping up with all the items that have been created
+    // pass in the integer which is the item id and it returns a market item.
+    // to fetch a market item, we only need the item id
+    mapping(uint256 => MarketItem) private idToMarketItem;
+
+    struct MarketItem {
+      uint256 tokenId;
+      address payable seller;
+      address payable owner;
+      uint256 price;
+      bool sold;
+    }
+
+    // have an event for when a market item is created.
+    // this event matches the MarketItem
+    event MarketItemCreated (
+      uint256 indexed tokenId,
+      address seller,
+      address owner,
+      uint256 price,
+      bool sold
+    );
