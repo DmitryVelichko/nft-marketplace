2801b4a59b80893b7dabac210e7da7bc105435ab
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-04 08:28:33 +0800

    Add Modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -8,3 +8,10 @@ const Modal = ({ header, body, footer, handleClose }) => {
   const modalRef = useRef(null);
   const { theme } = useTheme();
 
+  // check if it is cliked outside of modalRef
+  const handleClickOutside = (e) => {
+    if (modalRef.current && !modalRef.current.contains(e.target)) {
+      handleClose();
+    }
+  };
+
