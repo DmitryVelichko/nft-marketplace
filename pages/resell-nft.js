339c8261b5e0aaf84c18ed7a8e85705fe9115155
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-06 12:55:55 +0800

    Add Resell-nft page

--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -39,3 +39,16 @@ const ResellNFT = () => {
     );
   }
 
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-3/5 md:w-full">
+        <h1 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl">Resell NFT</h1>
+
+        <Input
+          inputType="number"
+          title="Price"
+          placeholder="Asset Price"
+          handleClick={(e) => setPrice(e.target.value)}
+        />
+
+        {image && <img className="rounded mt-4" width="350" src={image} />}
