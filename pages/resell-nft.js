8093aefaf066ab70f96bc63985d2cd65e7290ab5
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-06 12:55:36 +0800

    Add Resell-nft page

--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import { useEffect, useState, useContext } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios';
 
-const ResellNFT = () => (
-  <div>ResellNFT</div>
-);
+import { NFTContext } from '../context/NFTContext';
+import { Button, Input, Loader } from '../components';
 
-export default ResellNFT;
+const ResellNFT = () => {
+  const { createSale, isLoadingNFT } = useContext(NFTContext);
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const router = useRouter();
+  const { id, tokenURI } = router.query;
