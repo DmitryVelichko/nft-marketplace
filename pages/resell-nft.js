0c2bb33161128083c9cbcf2be3fba869a318185e
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-08 07:23:14 +0800

    Add nft-details  page

--- pages/resell-nft.js
+++ pages/resell-nft.js
@@ -1,43 +1,4 @@
-import { useEffect, useState, useContext } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
 
-import { NFTContext } from '../context/NFTContext';
-import { Button, Input, Loader } from '../components';
-
-const ResellNFT = () => {
-  const { createSale, isLoadingNFT } = useContext(NFTContext);
-  const [price, setPrice] = useState('');
-  const [image, setImage] = useState('');
-  const router = useRouter();
-  const { id, tokenURI } = router.query;
-
-  const fetchNFT = async () => {
-    if (!tokenURI) return;
-
-    const { data } = await axios.get(tokenURI);
-
-    setPrice(data.price);
-    setImage(data.image);
-  };
-
-  useEffect(() => {
-    fetchNFT();
-  }, [id]);
-
-  const resell = async () => {
-    await createSale(tokenURI, price, true, id);
-
-    router.push('/');
-  };
-
-  if (isLoadingNFT) {
-    return (
-      <div className="flexCenter" style={{ height: '51vh' }}>
-        <Loader />
-      </div>
-    );
-  }
 
   return (
     <div className="flex justify-center sm:px-4 p-12">
