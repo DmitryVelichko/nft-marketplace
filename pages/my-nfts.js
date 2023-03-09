270a38986ba147627cda7ced03a6c476bd337f01
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-09 08:03:46 +0800

    Add my nfts page

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -12,3 +12,32 @@ const MyNFTs = () => {
   const [nftsCopy, setNftsCopy] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [activeSelect, setActiveSelect] = useState('Recently Added');
+
+  useEffect(() => {
+    fetchMyNFTsOrCreatedNFTs('fetchMyNFTs')
+      .then((items) => {
+        setNfts(items);
+        setNftsCopy(items);
+        setIsLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    const sortedNfts = [...nfts];
+
+    switch (activeSelect) {
+      case 'Price (low to high)':
+        setNfts(sortedNfts.sort((a, b) => a.price - b.price));
+        break;
+      case 'Price (high to low)':
+        setNfts(sortedNfts.sort((a, b) => b.price - a.price));
+        break;
+      case 'Recently added':
+        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+        break;
+      default:
+        setNfts(nfts);
+        break;
+    }
+  }, [activeSelect]);
+
