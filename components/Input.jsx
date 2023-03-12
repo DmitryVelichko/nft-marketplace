37b67dbe4348b808ac976e8d31a25a1088d41cd2
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-12 22:53:36 +0800

    Add Input component

--- components/Input.jsx
+++ components/Input.jsx
@@ -23,3 +23,15 @@ const Input = ({ inputType, title, placeholder, handleClick }) => {
         : inputType === 'textarea' ? (
           <textarea rows={10} className="dark:bg-nft-black-1 bg-white border dark:border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3" placeholder={placeholder} onChange={handleClick} />
         )
+          : (
+            <input
+              className="dark:bg-nft-black-1 bg-white border dark:border-nft-black-1 border-nft-gray-2 rounded-lg w-full outline-none font-poppins dark:text-white text-nft-gray-2 text-base mt-4 px-4 py-3"
+              placeholder={placeholder}
+              onChange={handleClick}
+            />
+          )}
+    </div>
+  );
+};
+
+export default Input;
