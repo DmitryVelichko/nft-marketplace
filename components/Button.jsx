660a0a4c7cc47291674c6c9e919d0e47b83b55bc
Dmitry Velichko <soleil-2005@mail.ru>
2023-03-13 11:29:54 +0800

    Updated project

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+const primaryBtn = 'nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8 font-poppins font-semibold text-white';
+const outlineBtn = 'border border-nft-red-violet bg-transparent font-poppins font-semibold text-sm minlg:text-lg py-2 px-6 minlg:py-4 minlg:px-8';
 
-const Button = ({ classStyles, btnName, handleClick }) => (
+const Button = ({ btnName, classStyles, btnType, handleClick }) => (
   <button
     type="button"
-    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
+    className={
+        btnType === 'primary' ? `${primaryBtn} ${classStyles}` : `${outlineBtn} ${classStyles}`
+    }
     onClick={handleClick}
   >
     {btnName}
--- utils/makeId.js
+++ utils/makeId.js
@@ -10,3 +10,4 @@ export const makeid = (length) => {
 
   return result;
 };
+
